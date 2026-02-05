c002eb8983eebc6afb9a4b94a7addb66abdc599b
Mark Hollomon <mhhollomon@gmail.com>
2026-02-05 14:44:19 -0500

    first build

--- src/lib/atoms.ts
+++
@@ -1,4 +0,0 @@
-import {atom} from 'jotai'
-import { Color } from './color'
-
-export const colorAtom = atom<Color>(new Color(0, 0, 0))
--- src/lib/range.ts
+++
@@ -1,6 +0,0 @@
-export function range(start: number, stop: number, step = 1): number[] {
-    return Array.from(
-        { length: (stop - start) / step },
-        (_, i) => start + i * step
-    );
-}
