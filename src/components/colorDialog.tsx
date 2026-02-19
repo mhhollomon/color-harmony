9afee5c9757c4ae16acd23fdbfc8a8814f97df89
Mark Hollomon <mhhollomon@gmail.com>
2026-02-18 21:06:53 -0500

    start adding color sliders

--- src/components/colorDialog.tsx
+++ src/components/colorDialog.tsx
@@ -5,6 +5,7 @@ import './colorDialog.css'
 import { Color } from "~/lib/color";
 import { x_large } from '~/lib/icons';
 import ColorSwatch from '~/components/colorSwatch';
+import ColorSlider from './colorSlider';
 
 export interface ColorDialogProps {
     isOpen: boolean
@@ -14,8 +15,8 @@ export interface ColorDialogProps {
 export default function ColorDialog({ isOpen, setIsOpen }: ColorDialogProps) {
     const [colorString, setColorString] = useState('#000000');
     const { base_color, setBaseColor } = useGlobalStore();
-    const [ currentColor, setCurrentColor ] = useState(new color(base_color.toString()));
-    const [ errorMsg, setErrorMsg ] = useState('');
+    const [currentColor, setCurrentColor] = useState(new color(base_color.toString()));
+    const [errorMsg, setErrorMsg] = useState('');
     const dialogRef = useRef<HTMLDialogElement>(null);
 
 
@@ -33,6 +34,30 @@ export default function ColorDialog({ isOpen, setIsOpen }: ColorDialogProps) {
             console.log(err);
             setErrorMsg((err as Error).message);
         }
+    }
+    function onColorSliderChange(value: number, type: string, channel: string) {
+        console.log(`-- onColorSliderChange value = ${value} type = ${type} channel = ${channel}`)
+        const new_color = new color(currentColor)
+        if (type === 'rgb') {
+            switch (channel) {
+                case 'red':
+                    new_color.srgb.red = value / 255;
+                    break;
+
+                case 'blue':
+                    new_color.srgb.blue = value / 255;
+                    break;
+                case 'green' :
+                    new_color.srgb.green = value / 255;
+                    break
+                default:
+                    throw Error(`unkown channel '${channel} for type ${type}`)
+            }
+        }
+        setCurrentColor(new_color);
+        setColorString(new_color.toString());
+
+
     }
 
     useEffect(() => {
@@ -54,20 +79,30 @@ export default function ColorDialog({ isOpen, setIsOpen }: ColorDialogProps) {
 
     return (<>
         {/* Need the onClose to capture when 'esc' is pressed (rather than the close button). */}
-        <dialog className="color-dialog" ref={dialogRef} onClose={()=> setIsOpen(false)}>
+        <dialog className="color-dialog" ref={dialogRef} onClose={() => setIsOpen(false)}>
+            <header className='color-dialog__header'>Set Color
+                <button onClick={() => setIsOpen(false)}>{x_large}</button>
+            </header>
             <div className="color-dialog__content">
-                <header className='color-dialog__header'>Set Color
-                    <button onClick={() => setIsOpen(false)}>{x_large}</button>
-                </header>
 
                 <ColorSwatch color={new Color(currentColor)} className="color-dialog__color-swatch" index="dialog"
                     focusAction={false} infoAction={false}
                 />
-
-                <label htmlFor="color-string">Color</label>
-                <input type="text" value={colorString} id="color-string" onChange={onChange} />
-                {errorMsg ? <p className="color-dialog__error">{errorMsg}</p> : null}
-                <button onClick={handleSave}>Save</button>
+                <div>
+                    <label htmlFor="color-string">Color</label>
+                    <input type="text" value={colorString} id="color-string" onChange={onChange} />
+                    {errorMsg ? <p className="color-dialog__error">{errorMsg}</p> : null}
+                    <button onClick={handleSave}>Save</button>
+                </div>
+                <ColorSlider min={0} max={255} title="Red"
+                    value={Math.floor((currentColor.srgb.red ?? 0) * 255)}
+                    onChange={(value) => { onColorSliderChange(value, 'rgb', 'red') }} />
+                <ColorSlider min={0} max={255} title="Green"
+                    value={Math.floor((currentColor.srgb.green ?? 0) * 255)}
+                    onChange={(value) => { onColorSliderChange(value, 'rgb', 'green') }} />
+                <ColorSlider min={0} max={255} title="Blue"
+                    value={Math.floor((currentColor.srgb.blue ?? 0) * 255)}
+                    onChange={(value) => { onColorSliderChange(value, 'rgb', 'blue') }} />
             </div>
         </dialog>
     </>)
--- src/components/colorSlider.tsx
+++ src/components/colorSlider.tsx
@@ -1,17 +1,18 @@
 import { Slider } from "radix-ui";
 
-import './angleSlider.css'
+import './colorSlider.css'
 
-export type AngleSliderProps = {
+export type ColorSliderProps = {
     min: number,
     max: number,
+    title: string,
     step?: number,
     value: number,
     display?: number,
     onChange: (value: number) => void
 }
 
-export default function AngleSlider({ min, max, value, display, step, onChange }: AngleSliderProps) {
+export default function ColorSlider({ min, max, value, display, step, title, onChange }: ColorSliderProps) {
     /***** NOTE
      * The slide compoment doesn't seem to handle negative values well.
      * (E.g. a range from -40 to 40 doesn't work)
@@ -21,20 +22,22 @@ export default function AngleSlider({ min, max, value, display, step, onChange }
      * @param value is the value that should be used in the slider.
      */
     function onSliderChange(value: number[]) {
+        console.log(`-- colorslider '${title}' changing to ${value[0]}`)
         onChange(value[0]);
     }
     step ??= 5;
     display ??= value;
+    console.log(`-- colorslider current value = ${value}`)
     return (<>
-        <p className="angle-slider__label">Angle: {display}</p>
+        <p className="color-slider__label">{title}</p>
         <Slider.Root
-            className="angle-slider__root"
+            className="color-slider__root"
             onValueChange={onSliderChange}
             min={min} max={max} step={step} value={[value]}>
-            <Slider.Track className="angle-slider__track">
-                <Slider.Range className="angle-slider__range" />
+            <Slider.Track className="color-slider__track">
+                <Slider.Range className="color-slider__range" />
             </Slider.Track>
-            <Slider.Thumb className="angle-slider__thumb" />
+            <Slider.Thumb className="color-slider__thumb" />
         </Slider.Root>
     </>)
 }
